ec3405f60cb0135a0073682dc9659bcfc4dbef3d
Sujinunt Puangmalai <sujinunt555@hotmail.com>
2021-02-23 16:31:53 +0700

    Add ETH ecomerce test application

---
+++ truffle-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  networks: {
+    development: {
+     host: "127.0.0.1",     // Localhost (default: none)
+     port: 7545,            // Standard Ethereum port (default: none)
+     network_id: 5777,       // Any network (default: none)
+    },
+  },
+  compilers: {
+    solc: {
+      settings: {          // See the solidity docs for advice about optimization and evmVersion
+        optimizer: {
+          enabled: false,
+          runs: 200
+        },
+        evmVersion: "byzantium"
+      }
+    }
+  }
+}
